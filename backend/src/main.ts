706f1d0fa1911073fa33b6ab263f69533e6b5d0f
Septian Maulana <septiandev028@gmail.com>
2024-05-29 22:09:45 +0700

    be-feat: implement logic to create chat completions in openai module
    be-feat: register validation pipe in main
    
    be-build: install class validator and class transformer

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(app.get(ConfigService).getOrThrow('PORT'));
 }
 bootstrap();
--- backend/src/openai/openai.module.ts
+++ backend/src/openai/openai.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import OpenAI from 'openai';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { OpenaiController } from './openai.controller';
 import { OpenaiService } from './openai.service';
 
 @Module({
   controllers: [OpenaiController],
-  providers: [OpenaiService]
+  providers: [
+    OpenaiService,
+    {
+      provide: OpenAI,
+      useFactory: (configService: ConfigService) =>
+        new OpenAI({
+          apiKey: configService.getOrThrow('OPENAI_API_KEY'),
+        }),
+      inject: [ConfigService],
+    },
+  ],
+  imports: [ConfigModule],
 })
 export class OpenaiModule {}
