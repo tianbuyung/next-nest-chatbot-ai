6bad7bbef7403bbf6898ff5fc0cb9d447a19ef6a
Septian Maulana <septiandev028@gmail.com>
2024-05-29 21:29:43 +0700

    be-feat: generate openai module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { OpenaiModule } from './openai/openai.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), OpenaiModule],
   controllers: [],
   providers: [],
 })
